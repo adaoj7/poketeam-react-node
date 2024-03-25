a4f02b8b598bda0f69a791f147a62ee3934cd514
Adam Johnson <adaojohnson902@gmail.com>
2024-03-25 17:43:32 -0600

    pokemon sprites display through front and backend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 ﻿import express from "express";
 import morgan from "morgan";
 import ViteExpress from "vite-express";
+import axios from "axios";
 
 const app = express();
 const PORT = 4242;
@@ -9,6 +10,22 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(express.json());
+
+app.get(`/api/getPokemon`, async (req, res) => {
+    console.log(req.query.name.toLowerCase());
+    const lowerCaseName = req.query.name.toLowerCase();
+    // console.log(res);
+    try {
+        const data = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${lowerCaseName}`
+        );
+        console.log(data.data);
+        res.status(200).send(data.data);
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(400);
+    }
+});
 
 ViteExpress.listen(app, PORT, () =>
     console.log(`what is the answer? http://localhost:${PORT}`)
---
+++ src/Poketeam.tsx
@@ -0,0 +1,112 @@
+﻿import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Poketeam = () => {
+    // const [count, setCount] = useState(0);
+
+    interface PokemonData {
+        name: string;
+        id: number;
+        sprites: {
+            front_default: string;
+            front_shiny: string;
+        };
+    }
+
+    // const [searchCount, setSearchCount] = useState(0);
+    const [name, setName] = useState("");
+    const [pokemon, setPokemon] = useState<PokemonData | null>(null);
+    const [error, setError] = useState("");
+
+    // useEffect(() => {
+    //     const response: any = async () => {
+    //         try {
+    //             const response = await axios.get(`/api/getCount`);
+    //             setSearchCount(response.data);
+    //         } catch (err: any) {
+    //             console.log(err);
+    //         }
+    //     };
+    //     response();
+    // }, []);
+
+    const handleInputChange = (e: any) => {
+        setName(e.target.value);
+    };
+
+    const searchPokemon = async () => {
+        try {
+            console.log("hit");
+            // setSearchCount(searchCount + 1);
+            const response = await axios.get(`/api/getPokemon`, {
+                params: {
+                    name: name,
+                },
+            });
+            console.log(response);
+            if (!response) {
+                throw new Error(`Failed to fetch: ${response}`);
+            }
+            const data = await response.data;
+            //@ts-ignore
+            setPokemon(data);
+            setError("");
+        } catch (err: any) {
+            setError(err.message);
+            setPokemon(null);
+        }
+        // try {
+        //     await axios.put(`http://localhost:8000/api/update/${searchCount}`);
+        //     const newCount = searchCount + 1;
+        //     setSearchCount(newCount);
+        // } catch (err: any) {
+        //     console.log(err);
+        // }
+    };
+    return (
+        <>
+            <div className="flex h-[725px] flex-col content-center mt-40 justify-between">
+                <div>
+                    <h2 className="flex justify-center text-lg">
+                        This project is going to help me to build teams when I
+                        play through Pokemon games
+                    </h2>
+                    <div className="flex flex-col justify-center">
+                        <div className="flex justify-center p-4 mt-4 items-center">
+                            <input
+                                type="text"
+                                value={name}
+                                onChange={handleInputChange}
+                                placeholder="Enter Pokemon Name or Id"
+                                className="p-4 m-4"
+                            />
+                            <button
+                                className="btn flex"
+                                onClick={searchPokemon}
+                            >
+                                Search
+                            </button>
+                        </div>
+                        <br />
+                        {error && <p>Error: {error}</p>}
+                        {pokemon && (
+                            <div className="flex flex-row justify-center">
+                                <img
+                                    src={pokemon.sprites.front_default}
+                                    alt=""
+                                />
+                                <img src={pokemon.sprites.front_shiny} alt="" />
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-end">
+                    {/* <div>Pokemon generated:</div> */}
+                    <a href="https://pokeapi.co/">PokeAPI</a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Poketeam;
