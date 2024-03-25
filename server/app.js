3e45a1959bbe475e4c49f0fe4c5c0ba23f6a23a0
Adam Johnson <adaojohnson902@gmail.com>
2024-03-25 13:24:21 -0600

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   server/app.js 	new file:   src/App.css 	new file:   src/App.tsx 	new file:   src/assets/react.svg 	new file:   src/index.css 	new file:   src/main.tsx 	new file:   src/vite-env.d.ts 	new file:   tailwind.config.js 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ server/app.js
@@ -0,0 +1,15 @@
+﻿import express from "express";
+import morgan from "morgan";
+import ViteExpress from "vite-express";
+
+const app = express();
+const PORT = 4242;
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+app.use(express.json());
+
+ViteExpress.listen(app, PORT, () =>
+    console.log(`what is the answer? http://localhost:${PORT}`)
+);
